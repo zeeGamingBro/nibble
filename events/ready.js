d598ceda42ca90f76d0faa050455e8e682508e91
keli5 <46039037+keli5@users.noreply.github.com>
2023-11-16 15:09:51 -0600

    funny status message

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,14 @@
+const Eris = require("eris")
+
+/**
+ * 
+ * @param {Eris.Client} bot 
+ */
 module.exports = async (bot) => {
     console.log("Event handled: ready")
     console.log(`Logged into discord as ${bot.user.username}`)
+    bot.editStatus("online", {
+        name: "nickname updates",
+        type: 2
+    })
 }
