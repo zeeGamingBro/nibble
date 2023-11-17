9e2fb79a6d2b465b8f70e5d5b39c3c723ae2efe7
keli5 <46039037+keli5@users.noreply.github.com>
2023-11-16 19:07:06 -0600

    events status

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,36 @@
 const Eris = require("eris")
+const fs = require("fs")
+
+let changeInterval = undefined;
 
 /**
  * 
  * @param {Eris.Client} bot 
  */
 module.exports = async (bot) => {
-    console.log("Event handled: ready")
     console.log(`Logged into discord as ${bot.user.username}`)
+    
+    clearInterval(changeInterval) // We have no guarantee ready only fires once
+
+    let eventListing = fs.readdirSync("./events/")
+    eventListing.forEach((value, index, array) => {
+        array[index] = value.replace("/", "").replace("\\", "").replace(".js", "")
+        if (array[index] == "ready") delete array[index]
+    })
+    eventListing = eventListing.filter(n => n)
+
     bot.editStatus("online", {
-        name: "nickname updates",
+        name: `*yawn*`,
+        type: 0
+    })
+
+    changeInterval = setInterval(changeStatus, 2 * 60 * 1000, bot, eventListing)
+
+}
+
+function changeStatus(bot, events) {
+    bot.editStatus("online", {
+        name: `${events[Math.floor(Math.random()*events.length)]} events`,
         type: 2
     })
 }
